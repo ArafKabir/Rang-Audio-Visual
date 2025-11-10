ee13860ff7ca206085ca1b9f2bea919b9a9b58bb
Araf Kabir <kaziarfkabir3@gmail.com>
2025-11-10 01:10:27 -0700

    zoomed in error fixed(i guess 3)

--- app/root.tsx
+++ app/root.tsx
@@ -38,7 +38,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <html lang="en" className="scroll-smooth">
         <head>
             <meta charSet="utf-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+            />
+
             <meta name="description" content="Rang Audio Visual - LED screen rentals and event production" />
             <meta name="theme-color" content="#0E2348" />
 
